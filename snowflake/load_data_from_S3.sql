afb7116052286f9b02643802ee8457b8c70e5ec2
haminh6c2 <haminh6c2@gmail.com>
2025-04-24 21:42:21 +0000

    feat: creating STAGR, DATABASE, and SCHEMA

--- snowflake/load_data_from_S3.sql
+++ snowflake/load_data_from_S3.sql
@@ -1,23 +1,35 @@
+CREATE OR REPLACE STAGE my_s3_stage
+URL = 'your_url'
+CREDENTIALS = (
+  AWS_KEY_ID = 'your_key'
+  AWS_SECRET_KEY = 'your_secret'
+)
+FILE_FORMAT = (TYPE = 'CSV' FIELD_OPTIONALLY_ENCLOSED_BY = '"' SKIP_HEADER = 1);
+
+CREATE OR REPLACE DATABASE orchestra;
+USE orchestra;
+CREATE SCHEMA DEV;
+CREATE SCHEMA PROD;
+
 -- product_categories
-CREATE OR REPLACE TABLE dbt_raw.public.product_categories (
+CREATE OR REPLACE TABLE orchestra.dev.product_categories (
     product_category_key STRING,
     category_name STRING
 );
-COPY INTO dbt_raw.public.product_categories
+COPY INTO orchestra.dev.product_categories
   FROM @my_s3_stage/AdventureWorks_Product_Categories.csv;
 
 -- product_subcategories
-CREATE OR REPLACE TABLE dbt_raw.public.product_subcategories (
+CREATE OR REPLACE TABLE orchestra.dev.product_subcategories (
     product_subcategory_key STRING,
     subcategory_name STRING,
-    product_category_Key STRING
+    product_category_key STRING
 );
-COPY INTO dbt_raw.public.product_subcategories
+COPY INTO orchestra.dev.product_subcategories
   FROM @my_s3_stage/AdventureWorks_Product_Subcategories.csv;
 
 -- products
-
-CREATE OR REPLACE TABLE dbt_raw.public.products (
+CREATE OR REPLACE TABLE orchestra.dev.products (
     product_key STRING,
     product_subcategory_key STRING,
     product_sku STRING,
@@ -30,11 +42,11 @@ CREATE OR REPLACE TABLE dbt_raw.public.products (
     product_cost FLOAT,
     product_price FLOAT
 );
-COPY INTO dbt_raw.public.products
+COPY INTO orchestra.dev.products
   FROM @my_s3_stage/AdventureWorks_Products.csv;
 
- -- sales
-CREATE OR REPLACE TABLE dbt_raw.public.sales (
+-- sales
+CREATE OR REPLACE TABLE orchestra.dev.sales (
     order_date STRING,
     stock_date STRING,
     order_number STRING,
@@ -44,15 +56,15 @@ CREATE OR REPLACE TABLE dbt_raw.public.sales (
     order_line_item INT,
     order_quantity INT
 );
-COPY INTO dbt_raw.public.sales
+COPY INTO orchestra.dev.sales
   FROM @my_s3_stage/AdventureWorks_Sales_2017.csv;
 
 -- territories
-CREATE OR REPLACE TABLE dbt_raw.public.territories (
+CREATE OR REPLACE TABLE orchestra.dev.territories (
     sales_territory_key STRING,
     region STRING,
     country STRING,
     continent STRING
 );
-COPY INTO dbt_raw.public.territories
+COPY INTO orchestra.dev.territories
   FROM @my_s3_stage/AdventureWorks_Territories.csv;
